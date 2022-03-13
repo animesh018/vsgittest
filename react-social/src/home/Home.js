3a80e0fc69d3ab124cb7d809f8c4e4ac18c72b50
Animesh Srivastava <animeshsrivastava9789@gmail.com>
2022-03-14 04:55:27 +0530

    First commit

---
+++ react-social/src/home/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home-container">
+                <div className="container">
+                    <div className="graf-bg-container">
+                        <div className="graf-layout">
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                            <div className="graf-circle"></div>
+                        </div>
+                    </div>
+                    <h1 className="home-title">SocialAdda Welcomes You </h1>
+                    <h2>Click login or signup to proceed</h2>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
